2866d8d88979f5111e2560b75c327869fcc183a9
vijayanand17 <vijayanand.p@live.com>
2016-12-01 16:04:48 +0530

    Changes to accept license

--- lininfainstaller.sh
+++ lininfainstaller.sh
@@ -23,33 +23,44 @@ osUserName=$15
 storageName=$16
 storageKey=$17
 
+domainLicenseURL=$18
+
 #Usage
-if [ $# -ne 17 ]
+if [ $# -ne 18 ]
 then
-	lininfainstaller.sh domainHost domainName domainUser domainPassword nodeName nodePort dbType dbName dbUser dbPassword dbHost dbPort sitekeyKeyword joinDomain  osUserName storageName storageKey
+	lininfainstaller.sh domainHost domainName domainUser domainPassword nodeName nodePort dbType dbName dbUser dbPassword dbHost dbPort sitekeyKeyword joinDomain  osUserName storageName storageKey domainLicenseURL
 fi
 
 apt-get update &>/dev/null
 
-CLOUD_SUPPORT_ENABLE=1
-
 dbAddress=$dbHost:$dbPort
 hostName=`hostname`
 
+informaticaopt=/opt/Informatica
+infainstallerloc=$informaticaopt/Archive/server
+utilityHome=$informaticaopt/Archive/Utilities
 
-infainstallerloc=/opt/Informatica/Archive/server
-infainstallionloc=\\/home\\/$osUserName\\/Informatica\\/10.0.0
+infainstallionlocown=/home/Informatica
+mkdir -p $infainstallionlocown/10.1.1
 
+infainstallionloc=\\/home\\/Informatica\\/10.1.1
 defaultKeyLocation=$infainstallionloc\\/isp\\/config\\/keys
 
-utilityHome=/opt/Informatica/Archive/Utilities
-
-JAVA_HOME="/opt/Informatica/Archive/server/source/java"
-export JAVA_HOME		
-PATH="$JAVA_HOME/bin":"$PATH"
+JRE_HOME="$infainstallerloc/source/java/jre"
+export JRE_HOME		
+PATH="$JRE_HOME/bin":"$PATH"
 export PATH
 
-chmod -R 777 $JAVA_HOME
+chmod -R 777 $JRE_HOME
+
+
+CLOUD_SUPPORT_ENABLE=1
+if [ "$domainLicenseURL" != "nolicense" && $joinDomain -eq 0 ]
+then
+	CLOUD_SUPPORT_ENABLE=0
+	java -jar iadutility.jar downloadHttpUrlFile -url $domainLicenseURL -localpath $informaticaopt
+fi
+
 
 createDomain=1
 if [ $joinDomain -eq 1 ]
@@ -62,11 +73,13 @@ else
     java -jar iadutility.jar createAzureFileShare -storageaccesskey $storageKey -storagename $storageName
 fi
 
+
 apt-get install cifs-utils
 mountDir=/mnt/infaaeshare
 mkdir $mountDir
 mount -t cifs //$storageName.file.core.windows.net/infaaeshare $mountDir -o vers=3.0,username=$storageName,password=$storageKey,dir_mode=0777,file_mode=0777
 echo //$storageName.file.core.windows.net/infaaeshare $mountDir cifs vers=3.0,username=$storageName,password=$storageKey,dir_mode=0777,file_mode=0777 >> /etc/fstab
+
 
 sed -i s/^USER_INSTALL_DIR=.*/USER_INSTALL_DIR=$infainstallionloc/ $infainstallerloc/SilentInput.properties
 
@@ -117,8 +130,7 @@ sed -i s/^DOMAIN_CNFRM_PSSWD=.*/DOMAIN_CNFRM_PSSWD=$domainPassword/ $infainstall
 cd $infainstallerloc
 echo Y Y | sh silentinstall.sh 
 
-infainstallionlocown=/home/$osUserName/Informatica
 
 chown -R $osUserName $infainstallionlocown
-chown -R $osUserName /opt/Informatica 
-chown -R $osUserName /mnt/infaaeshare
+chown -R $osUserName $informaticaopt 
+chown -R $osUserName $mountDir
